6f20225a5a2e0d001c22f67c9f4d220724e98380
LeeJunSang <dev.enjoy.art@gmail.com>
2023-02-07 08:38:41 +0900

    auto push 20230207 0838

--- workspace/day01/oper.js
+++ workspace/day01/oper.js
@@ -1,21 +1,23 @@
-//  false로 취급되는 값: "", 0, null, undefined
-function testFalse(value){
-    let data = value;
+// false로 취급되는 값: "", 0, null, undefined
+function testFalse(){
+    let data = undefined;
     let result;
     if(data){
-        reuslt = "TRUE";
-    }else {
+        result = "TRUE";
+
+    }else{
         result = "FALSE";
+
     }
     console.log(result);
 }
 
-function compareType() {
+function compareType(){
     let data1 = "1", data2 = 1;
 
-    console.log(data1 == data2);    // 값만 같으면 true
-    console.log(data1 === data2);   // 타입까지 같아야 true
+    console.log(data1 == data2);    //값만 같으면 true
+    console.log(data1 === data2);   //타입까지 같아야 true
 }
 
-testFalse(undefined);
+testFalse();
 compareType();
--- workspace/day01/script.js
+++ workspace/day01/script.js
@@ -1,6 +1,16 @@
-//  script-file.html
+// script-file.html
+// url.html
 
 function changeImage(object){
     let status = object.classList[0];
-    status == "on" ? "image/icon2.png";
+    
+    if(status == "on"){
+        object.classList.replace("on", "off");
+        object.src = "image/icon3.png";
+        return;
+    }
+
+    object.classList.replace("off", "on");
+    object.src = "image/icon4.png";
+
 }
